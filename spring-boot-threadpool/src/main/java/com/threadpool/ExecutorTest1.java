432a213d7f8e29fdef3785548b8ea59060437e76
yaoyinong <2587403163@qq.com>
2022-07-18 22:18:47 +0800

    线程池

--- spring-boot-threadpool/src/main/java/com/threadpool/ExecutorTest1.java
+++ spring-boot-threadpool/src/main/java/com/threadpool/ExecutorTest1.java
@@ -1,5 +1,7 @@
 package com.threadpool;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -11,6 +13,7 @@ import java.util.concurrent.TimeUnit;
  * @description jdk自带的线程池测试
  * 缺点：大部分jdk自带线程池实现类内部都使用了无界队列，有可能会导致内存溢出
  */
+@Slf4j
 public class ExecutorTest1 {
 
     public static void main(String[] args) {
@@ -24,9 +27,7 @@ public class ExecutorTest1 {
         ScheduledExecutorService executorService = Executors.newScheduledThreadPool(2);
         for (int i = 0; i < 10; i++) {
             int finalI = i;
-            executorService.schedule(() ->
-                            System.out.println(Thread.currentThread().getName() + "：" + finalI)
-                    ,3, TimeUnit.SECONDS);
+            executorService.schedule(() -> log.info(Thread.currentThread().getName() + "：" + finalI), 3, TimeUnit.SECONDS);
         }
     }
 
